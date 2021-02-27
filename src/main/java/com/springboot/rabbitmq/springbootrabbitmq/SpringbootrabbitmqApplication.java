232b9aa988523c780784b4856975f238f554521d
DESKTOP-E7L6HLO\Noyan <sngermiyanoglu@hotmail.com>
2021-02-27 14:22:42 +0300

    Task 1 : Initial commit

---
+++ src/main/java/com/springboot/rabbitmq/springbootrabbitmq/SpringbootrabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.rabbitmq.springbootrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootrabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootrabbitmqApplication.class, args);
+    }
+
+}
